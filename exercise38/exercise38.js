ad7314f56f9a4225c01b24c3f13e6571a10def4b
Eduardo Andrade Orthmann <orthmann2811@gmail.com>
2023-07-18 21:23:12 -0300

    add ravin

---
+++ exercise38/exercise38.js
@@ -0,0 +1,15 @@
+// Open links in mobiles
+function handleSmallScreens() {
+  document.querySelector('.navbar-toggler')
+    .addEventListener('click', () => {
+      let navbarMenu = document.querySelector('.navbar-menu')
+
+      if (!navbarMenu.classList.contains('active')) {
+        navbarMenu.classList.add('active')
+      } else {
+        navbarMenu.classList.remove('active')
+      }
+    })
+}
+
+handleSmallScreens()
