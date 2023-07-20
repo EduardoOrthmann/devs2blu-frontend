2f169c972656d6e6a21a4bca12680a8e71e130c5
Eduardo Andrade Orthmann <orthmann2811@gmail.com>
2023-07-19 21:38:07 -0300

    remove unecessary html

--- exercise38/exercise38.js
+++ exercise38/exercise38.js
@@ -18,29 +18,9 @@ window.addEventListener('resize', () => {
   }
 });
 
-const logo = document.querySelector('.logo');
-const logoExpand = document.querySelector('.logo-expand');
 const discover = document.querySelector('.discover');
 const mainContainer = document.querySelector('.main-container');
 const trending = document.querySelector('.trending');
-const video = document.querySelector('.video');
-const videoStream = document.querySelector('.video-stream');
-const videoName = document.querySelector('.video-name');
-const videoBy = document.querySelector('.video-by');
-const authorImg = document.querySelector('.author-img');
-const videoPTitle = document.querySelector('.video-p-title');
-const videoPName = document.querySelector('.video-p-name');
-const videoDetail = document.querySelector('.video-detail');
-
-logo.addEventListener('click', () => {
-  mainContainer.classList.remove('show');
-  mainContainer.scrollTop = 0;
-});
-
-logoExpand.addEventListener('click', () => {
-  mainContainer.classList.remove('show');
-  mainContainer.scrollTop = 0;
-});
 
 discover.addEventListener('click', () => {
   mainContainer.classList.remove('show');
@@ -56,24 +36,4 @@ trending.addEventListener('click', () => {
   trending.classList.add('is-active');
 });
 
-video.addEventListener('click', () => {
-  mainContainer.classList.add('show');
-  mainContainer.scrollTop = 0;
-  sidebarLinks.forEach((link) => {
-    link.classList.remove('is-active');
-  });
-  trending.classList.add('is-active');
-});
 
-video.addEventListener('click', () => {
-  const source = video.querySelector('source').getAttribute('src');
-  const title = video.querySelector('.video-name').textContent;
-  const person = video.querySelector('.video-by').textContent;
-  const img = video.querySelector('.author-img').getAttribute('src');
-  videoStream.pause();
-  videoStream.querySelector('source').setAttribute('src', source);
-  videoStream.load();
-  videoPTitle.textContent = title;
-  videoPName.textContent = person;
-  authorImg.setAttribute('src', img);
-});
