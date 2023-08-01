2f5b29ac4302c591c25176a38415223435eaadbb
Eduardo Andrade Orthmann <orthmann2811@gmail.com>
2023-07-31 21:49:52 -0300

    add new UI

--- exercise38/exercise38.js
+++ exercise38/exercise38.js
@@ -1,13 +1,44 @@
-const sidebarLinks = document.querySelectorAll('.sidebar-link');
-
-sidebarLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    sidebarLinks.forEach((link) => {
-      link.classList.remove('is-active');
-    });
-    link.classList.add('is-active');
-  });
+document.querySelector(".jsFilter").addEventListener("click", () => {
+  document.querySelector(".filter-menu").classList.toggle("active");
 });
+
+document.querySelector(".grid").addEventListener("click", () => {
+  document.querySelector(".list").classList.remove("active");
+  document.querySelector(".grid").classList.add("active");
+  document.querySelector(".products-area-wrapper.gridView").classList.add("active");
+  document.querySelector(".products-area-wrapper.tableView").classList.remove("active");
+
+  document.querySelector("#filterButtonWrapper").style.display = "";
+  document.querySelector("#searchBar").style.display = "";
+});
+
+document.querySelector(".list").addEventListener("click", () => {
+  document.querySelector(".list").classList.add("active");
+  document.querySelector(".grid").classList.remove("active");
+  document.querySelector(".products-area-wrapper.gridView").classList.remove("active");
+  document.querySelector(".products-area-wrapper.tableView").classList.add("active");
+
+  document.querySelector("#filterButtonWrapper").style.display = "none";
+  document.querySelector("#searchBar").style.display = "none";
+});
+
+const modeSwitch = document.querySelector('.mode-switch');
+modeSwitch.addEventListener('click', () => {
+  document.documentElement.classList.toggle('light');
+  modeSwitch.classList.toggle('active');
+});
+
+// TODO: implement later when it happens to be implemented other pages
+// const sidebarLinks = document.querySelectorAll('.sidebar-link');
+
+// sidebarLinks.forEach((link) => {
+//   link.addEventListener('click', () => {
+//     sidebarLinks.forEach((link) => {
+//       link.classList.remove('is-active');
+//     });
+//     link.classList.add('is-active');
+//   });
+// });
 
 window.addEventListener('resize', () => {
   const sidebar = document.querySelector('.sidebar');
@@ -17,23 +48,3 @@ window.addEventListener('resize', () => {
     sidebar.classList.add('collapse');
   }
 });
-
-const discover = document.querySelector('.discover');
-const mainContainer = document.querySelector('.main-container');
-const trending = document.querySelector('.trending');
-
-discover.addEventListener('click', () => {
-  mainContainer.classList.remove('show');
-  mainContainer.scrollTop = 0;
-});
-
-trending.addEventListener('click', () => {
-  mainContainer.classList.add('show');
-  mainContainer.scrollTop = 0;
-  sidebarLinks.forEach((link) => {
-    link.classList.remove('is-active');
-  });
-  trending.classList.add('is-active');
-});
-
-
