72a40e61342b94b739fe107c758394d72eb5d265
Eduardo A. Orthmann <92002832+sSn0wy@users.noreply.github.com>
2023-08-02 04:26:31 -0300

    add search and filter functionalities

--- exercise38/exercise38.js
+++ exercise38/exercise38.js
@@ -1,3 +1,4 @@
+// TOGGLES
 document.querySelector(".jsFilter").addEventListener("click", () => {
   document.querySelector(".filter-menu").classList.toggle("active");
 });
@@ -47,4 +48,94 @@ window.addEventListener('resize', () => {
   } else {
     sidebar.classList.add('collapse');
   }
+});
+
+const apiURL = 'https://64b70d31df0839c97e166026.mockapi.io/api/products';
+
+async function getProducts() {
+  try {
+    const response = await fetch(apiURL);
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+function renderProducts(products) {
+  const productsArea = document.querySelector('.products-area-wrapper.gridView');
+  productsArea.innerHTML = '';
+
+  if (products.length === 0) {
+    productsArea.innerHTML = '<p>Nenhum produto encontrado</p>';
+    return;
+  }
+
+  products.forEach((product) => {
+    const productCard = document.createElement('div');
+    productCard.classList.add('products-row');
+    productCard.innerHTML = `
+    <button class="cell-more-button">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="feather feather-more-vertical">
+          <circle cx="12" cy="12" r="1" />
+          <circle cx="12" cy="5" r="1" />
+          <circle cx="12" cy="19" r="1" />
+      </svg>
+    </button>
+    <div class="product-cell image">
+        <div class="product-image">
+            <img src="${product.image}" alt="product">
+            <span class="product-description">${product.description}</span>
+        </div>
+        <span>${product.name}</span>
+    </div>
+    <div class="product-cell"><span class="cell-label">Categoria:</span>${product.category}</div>
+    <div class="product-cell"><span class="cell-label">Preço:</span>R$ ${product.price.toFixed(2)}</div>
+    <div class="product-cell"><button type="button" class="btn">Adicionar</button></div>
+    `;
+
+    productsArea.appendChild(productCard);
+  });
+}
+
+async function filterProducts() {
+  const products = await getProducts();
+  const filter = document.querySelector('#filterProductInput').value;
+
+  if (filter === 'all') {
+    renderProducts(products);
+    return;
+  }
+
+  const filteredProducts = products.filter(product => product.category === filter);
+  renderProducts(filteredProducts);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  filterProducts();
+});
+
+document.querySelector('#filterApplyBtn').addEventListener('click', async () => {
+  filterProducts();
+});
+
+document.querySelector('#filterResetBtn').addEventListener('click', async () => {
+  document.querySelector('#filterProductInput').value = 'all';
+  filterProducts();
+});
+
+document.querySelector('#searchBar').addEventListener('keyup', async () => {
+  const products = await getProducts();
+  const search = document.querySelector('#searchBar').value;
+
+  if (search === '') {
+    renderProducts(products);
+    return;
+  }
+
+  const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+  renderProducts(filteredProducts);
 });
