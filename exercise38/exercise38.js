5e765697cdc85b3d9bf9ad432162c33685d63754
Eduardo <orthmann2811@gmail.com>
2023-08-03 17:35:30 -0300

    add delete order

--- exercise38/exercise38.js
+++ exercise38/exercise38.js
@@ -67,6 +67,16 @@ document.addEventListener('DOMContentLoaded', async () => {
   renderOrders(orders);
 });
 
+document.addEventListener('click', async (event) => {
+  const deleteBtn = event.target.closest('.delete-order-btn');
+  if (deleteBtn) {
+    const orderId = deleteBtn.dataset.orderId;
+    await deleteOrder(orderId);
+    const updatedOrders = await getOrders();
+    renderOrders(updatedOrders);
+  }
+});
+
 document
   .querySelector('#filterApplyBtn')
   .addEventListener('click', async () => {
@@ -329,7 +339,7 @@ async function postOrder(order) {
 
     const orders = await getOrders();
     renderOrders(orders);
-    if (orders.length === 0) document.querySelector('.list').click();
+    if (orders.length === 1) document.querySelector('.list').click();
   } catch (error) {
     console.error(error);
     alert('Erro ao adicionar pedido');
@@ -364,6 +374,14 @@ function renderOrders(orders) {
     </div>
     <div class="product-cell sales">${order.quantity}</div>
     <div class="product-cell price">R$ ${order.total_price.toFixed(2)}</div>
+    <div class="product-cell actions">
+      <button class="btn delete-order-btn" data-order-id="${order.id}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        </svg>
+      </button>
+    </div>
     `;
 
     ordersWrapper.appendChild(orderCard);
@@ -376,4 +394,15 @@ function renderOrders(orders) {
 
   const ordersTotalElement = document.querySelector('#totalPrice');
   ordersTotalElement.innerText = `R$ ${ordersTotal.toFixed(2)}`;
+}
+
+async function deleteOrder(id) {
+  try {
+    await fetch(`${apiURL}/orders/${id}`, {
+      method: 'DELETE',
+    });
+  } catch (error) {
+    console.error(error);
+    alert('Erro ao deletar pedido');
+  }
 }
